33d7663a110823e4b9ec7f2058fd99ce187f08bd
Andrea Minetti <andrea.minetti@wavein.ch>
2021-02-22 15:41:28 +0100

    Added langs and cleaned Codegen

--- src/main/g8/project/Codegen.scala
+++ src/main/g8/project/Codegen.scala
@@ -2,7 +2,8 @@ import sbt.Keys._
 import sbt._
 
 object Codegen {
-  lazy val generateModel = taskKey[Seq[File]]("gen-tables")
+
+  lazy val generateModel = taskKey[Boolean]("gen-tables")
   lazy val generateModelTask = Def.sequential(
     clean,
     Def.task{
@@ -10,21 +11,13 @@ object Codegen {
 
       val outputDir = (dir / "main" / "scala").getPath // place generated files in sbt's sources folder
 
-      val fname = outputDir + "/ch/wsl/box/generated/Entities.scala"
-      val ffname = outputDir + "/ch/wsl/box/generated/FileTables.scala"
-      val rname = outputDir + "/ch/wsl/box/generated/GeneratedRoutes.scala"
-      val registryname = outputDir + "/ch/wsl/box/generated/EntityActionsRegistry.scala"
-      val filename = outputDir + "/ch/wsl/box/generated/FileRoutes.scala"
-      val regname = outputDir + "/ch/wsl/box/generated/GenRegistry.scala"
-      val generatedFiles = Seq(file(fname),file(ffname),file(rname),file(registryname),file(filename),file(regname))    //include the generated files in the sbt project
-
       val s = streams.value
       val cp = (dependencyClasspath in Compile).value
       val out = (sbt.Keys.`package` in Compile).value
       val files = cp.files ++ Seq(out)
       runner.value.run("ch.wsl.box.model.Migrate", files, Seq(), s.log).failed foreach (sys error _.getMessage)
       runner.value.run("ch.wsl.box.codegen.CustomizedCodeGenerator", files, Array(outputDir), s.log).failed foreach (sys error _.getMessage)
-      generatedFiles
+      true
     }
   )
 }
